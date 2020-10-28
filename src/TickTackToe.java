119a9fe60abdf7c42c33cf82026af16f6fcc10f8
BuildTools <unconfigured@null.spigotmc.org>
2020-10-28 16:00:02 +0100

    player1Won & player2Won created (aka chek in the array for 3 of the same number in a row)

--- src/TickTackToe.java
+++ src/TickTackToe.java
@@ -11,9 +11,12 @@ public class TickTackToe {
 
     //int array that represents the 9 slots in a tickTackToe game
     //if the first player presses the array index of the press will be set to a value of 1 while the 2nd players to 2
-    private int[] slots = new int[9];
+    private final int[] slots = new int[9];
 
     private boolean gameOver;
+
+    //boolean that switches between true and false || true = player1 || false = player2
+    private boolean PlayerSwitch;
 
     public TickTackToe(int[] slots) {
 
@@ -35,6 +38,58 @@ public class TickTackToe {
 
         gameOver = false;
 
+    }
+
+    public boolean player1Won(){
+        //player 1 full row check
+        if(
+                //horizontal rows
+                slots[0] == 1 && slots[1] == 1 && slots[2] == 1 || //if the first horizontal row is filled with 1
+                slots[3] == 1 && slots[4] == 1 && slots[5] == 1 || //if the second horizontal row is filled with 1
+                slots[6] == 1 && slots[7] == 1 && slots[8] == 1 || //if the third horizontal row is filled with 1
+                //vertical rows
+                slots[0] == 1 && slots[3] == 1 && slots[6] == 1 || //if the first vertical row is filled with 1
+                slots[1] == 1 && slots[4] == 1 && slots[7] == 1 || //if the second vertical row is filled with 1
+                slots[2] == 1 && slots[5] == 1 && slots[8] == 1 || //if the third vertical row is filled with 1
+                // sideways \ row
+                slots[0] == 1 && slots[4] == 1 && slots[8] == 1 || //if the row is filled with 1
+                // sideways / row
+                slots[6] == 1 && slots[4] == 1 && slots[2] == 1//if the row is filled with 1
+        ){
+
+            //There was a 3 in a row of 1s
+            return true;
+        }else{
+
+            //if there is not a 3 in a row of 1s the game will not be over
+            return false;
+        }
+    }
+    public boolean player2Won(){
+        //player2 full row check
+
+        if(
+                //horizontal rows
+                slots[0] == 2 && slots[1] == 2 && slots[2] == 2 || //if the first horizontal row is filled with 1
+                slots[3] == 2 && slots[4] == 2 && slots[5] == 2 || //if the second horizontal row is filled with 1
+                slots[6] == 2 && slots[7] == 2 && slots[8] == 2 || //if the third horizontal row is filled with 1
+                //vertical rows
+                slots[0] == 2 && slots[3] == 2 && slots[6] == 2 || //if the first vertical row is filled with 1
+                slots[1] == 2 && slots[4] == 2 && slots[7] == 2 || //if the second vertical row is filled with 1
+                slots[2] == 2 && slots[5] == 2 && slots[8] == 2 || //if the third vertical row is filled with 1
+                // sideways \ row
+                slots[0] == 2 && slots[4] == 2 && slots[8] == 2 || //if the row is filled with 1
+                // sideways / row
+                slots[6] == 2 && slots[4] == 2 && slots[2] == 2//if the row is filled with 1
+        ){
+
+            //There was a 3 in a row of 1s
+            return true;
+        }else{
+
+            //if there is not a 3 in a row of 1s the game will not be over
+            return false;
+        }
     }
 
 }
